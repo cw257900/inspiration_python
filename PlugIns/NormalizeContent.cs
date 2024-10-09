e4aaed6adf4ef1a56b9cf43219a379721060bfe1
cw257900 <cw2579@gmail.com>
2024-10-09 15:00:45 -0500

    add unstructure libraries

--- PlugIns/NormalizeContent.cs
+++ PlugIns/NormalizeContent.cs
@@ -11,6 +11,18 @@ from unstructured.partition.html import partition_html
 from unstructured.partition.pptx import partition_pptx
 from unstructured.staging.base import dict_to_elements, elements_to_json
 
+
+from Utils import Utils
+utils = Utils()
+
+DLAI_API_KEY = utils.get_dlai_api_key()
+DLAI_API_URL = utils.get_dlai_url()
+
+s = UnstructuredClient(
+    api_key_auth=DLAI_API_KEY,
+    server_url=DLAI_API_URL,
+)
+
 #sample test
 data = '{"name":"John", "age":30}'
 parsed_data = json.loads(data)
