86c3905feb19828988a510e678342ecf1c4ea1ec
cw257900 <cw2579@gmail.com>
2024-10-12 20:21:19 -0500

    rag

--- Rag/rag.py
+++ Rag/rag.py
@@ -8,15 +8,28 @@ load_dotenv()
 API_KEY_OPENAI=os.getenv("API_KEY_OPENAI")
 API_TOKEN_HUGGENFACE=os.getenv("")
 
+
+
+
+
+
+
+
 def main():
     st.set_page_config(page_title="Chat with Connie", page_icon=":books:")
-    st.header("Chat with Connie :books: ")
+    st.header("Chat with Connie :books: , ")
     st.text_input ("Ask Connie:")
 
     with st.sidebar:
         st.subheader("Upload documents")
-        st.file_uploader("Upload PDFs here, click on process to continue" )
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload PDFs here, click on process to continue", accept_multiple_files=True )
+        if st.button("Process"):
+            #get pdf text
+
+            #get text chuncks
+
+            #create vector store
+            
 
     if __name__ == '__main__':
         main()
