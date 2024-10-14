3b30ff883985ca89dc4c2562add8614426cf22b9
cw257900 <cw2579@gmail.com>
2024-10-14 02:31:09 -0500

    rag

---
+++ Rag/.venv/use_cases/pdfs/utils/pdf_processor.py
@@ -0,0 +1,37 @@
+import os
+import traceback 
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+
+
+
+# Function to read PDF and extract text from all pages
+def get_pdf_text(pdf_file_path):
+    with open(pdf_file_path, 'rb') as file:
+        reader = PdfReader(file)
+        text = ""
+        # Iterate through all pages and extract text
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+# Function to split the extracted text into chunks for better embedding
+def get_text_chunks(pdf_file_path):
+
+    if not pdf_file_path or not os.path.exists(pdf_file_path):
+        print(pdf_file_path)
+        raise ValueError(f"Invalid PDF file path: {pdf_file_path}")
+
+    text = get_pdf_text(pdf_file_path)
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
