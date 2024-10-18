2a55b3dd0254f66fbe10a4f7fcb0e35217c6f569
cw257900 <77124453+cw257900@users.noreply.github.com>
2024-10-18 00:23:18 -0500

    processor.py

--- Rag/with_weaviate/utils/pdf_processor.py
+++ Rag/with_weaviate/utils/pdf_processor.py
@@ -1,23 +1,13 @@
 import os
 import traceback 
-#from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 
+load_dotenv()
 
-# Function to read PDF and extract text from all pages
-def get_pdf_text(pdf_file_path):
-    """
-    with open(pdf_file_path, 'rb') as file:
-        reader = PdfReader(file)
-        text = ""
-        # Iterate through all pages and extract text
-        for page_num in range(len(reader.pages)):
-            page = reader.pages[page_num]
-            text += page.extract_text()
-    return text
-   """
    
 # Function to split the extracted text into chunks for better embedding
 def get_checked_doc(pdf_file_path):
@@ -25,8 +15,6 @@ def get_checked_doc(pdf_file_path):
 
     if not pdf_file_path or not os.path.exists(pdf_file_path):
         raise ValueError(f"Invalid PDF file path: {pdf_file_path}")
-
-    text = get_pdf_text(pdf_file_path)
    
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -36,3 +24,14 @@ def get_checked_doc(pdf_file_path):
     docs = PyPDFLoader(pdf_file_path).load_and_split(text_splitter)
     print(f"1. Number of chunks from pdf: {len(docs)}")
     return docs
+
+def main ():
+
+    pdf_file_path = os.env("LOCAL_FILE_INPUT_PATH")
+    print (pdf_file_path)
+    doc = pdf_file_path(pdf_file_path)
+    print (pdf_file_path)
+    
+
+def __main__():
+    main()
