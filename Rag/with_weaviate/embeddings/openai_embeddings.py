17bb696b12e18e206a6d801e3e536d6951c256c1
cw257900 <77124453+cw257900@users.noreply.github.com>
2024-10-19 14:55:52 -0500

    embeddings

--- Rag/with_weaviate/embeddings/openai_embeddings.py
+++ Rag/with_weaviate/embeddings/openai_embeddings.py
@@ -1,37 +1,17 @@
 
-from dotenv import load_dotenv
 import os
 import sys
-
-
 from langchain_openai import OpenAIEmbeddings 
 # Add the parent directory (or wherever "with_pinecone" is located) to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from configs import configs
+from dotenv import load_dotenv
 
-
-"""""
-# Explicitly specify the path to the .env file
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
-
-# Load environment variables from the specified .env file
-load_dotenv(dotenv_path)
-
-# Check if the environment variables are being loaded correctly
-print(f"dotenv_path: {dotenv_path}")
-pinecone_api_key = os.getenv("PINECONE_API_KEY")
-pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
-
-# Verify the loaded environment variables
-print(f"pinecone_api_key: {pinecone_api_key}")
-print(f"pinecone_index_name: {pinecone_index_name}")
-
-"""
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Set API keys and Weaviate URL from environment variables
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 
-print(f"0.1. embeddings")
+print(f"0.1. embeddings initiated from openai_embeddings.py")
 
