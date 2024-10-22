cbe35aff7c6ade148749e6b9cd845cacb9874260
cw257900 <77124453+cw257900@users.noreply.github.com>
2024-10-22 14:43:07 -0500

    rag

--- Rag/with_weaviate/configs/configs.py
+++ Rag/with_weaviate/configs/configs.py
@@ -1,6 +1,6 @@
 
 
 
-WEAVIATE_STORE_NAME="PDF_COLLECTION"
+WEAVIATE_STORE_NAME="PDF_COLLECTION_WITH_TRASFORMER"
 WEAVIATE_STORE_DESCRIPTION="PDF Collection with embeddings by Weaviate"
-text2vec_model="text-embedding-3-large"
+text2vec_model="sentence-transformers/all-MiniLM-L6-v2"
